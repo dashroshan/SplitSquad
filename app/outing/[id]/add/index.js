f3a96112cfc4d52976fb9223babd9de6f15fa96d
Roshan Dash <roshan1337d@gmail.com>
2024-02-28 16:24:00 +0530

    Spent by selection

--- app/outing/[id]/add/index.js
+++ app/outing/[id]/add/index.js
@@ -1,5 +1,12 @@
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, Text, TextInput } from "react-native";
+import {
+    Modal,
+    Pressable,
+    ScrollView,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
 import TopBar from "../../../../components/TopBar";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -18,19 +25,25 @@ function index(props) {
                     placeholder="Example: Movie tickets"
                     cursorColor="black"
                     placeholderTextColor="lightgray"
-                    className="border-gray-900 border-b-2 pb-1 my-3"
+                    className="border-black border-b-2 pb-1 my-3"
                 />
 
                 {/* Amount */}
-                <Text className="font-semibold text-lg mt-5">Amount</Text>
-                <TextInput
-                    placeholder="Example: 500"
-                    cursorColor="black"
-                    placeholderTextColor="lightgray"
-                    className="border-gray-900 border-b-2 pb-1 my-3"
-                    inputMode="decimal"
-                />
-
+                <Text className="font-semibold text-lg mt-5 mb-3">
+                    Spent by
+                </Text>
+                <Pressable onPress={() => console.log("Pressed")}>
+                    <View className="border-black border-2 pl-3 flex-row justify-between items-center">
+                        <Text></Text>
+                        <View className="bg-black p-2 pl-4 pr-3">
+                            <AntDesign
+                                name="caretdown"
+                                size={26}
+                                color="white"
+                            />
+                        </View>
+                    </View>
+                </Pressable>
             </ScrollView>
         </>
     );
